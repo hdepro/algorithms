fe3ea7b5a002ac75da865b13bec44d0c441885c3
heben <1111>
2017-04-02 22:39:09 +0800

    343. Integer Break

--- leetcode/343. Integer Break.js
+++ leetcode/343. Integer Break.js
@@ -8,5 +8,20 @@
  * @return {number}
  */
 var integerBreak = function(n) {
-
+    var section = parseInt(n/3),mod = n%3;
+    if(section<2) return Math.floor(n/2)*Math.ceil(n/2);
+    var product=1;
+    if(mod==1) {
+        product=4;
+        section--;
+    }else if(mod==2){
+        product=2;
+    }
+    for(var i=0;i<section;i++){
+       product*=3;
+    }
+    return product;
 };
+console.log(integerBreak(2));
+console.log(integerBreak(5));
+console.log(integerBreak(10));
