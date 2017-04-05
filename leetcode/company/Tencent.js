29a6d2454bbebc66963271a5512deb29289cfc49
heben <1111>
2017-04-05 21:27:30 +0800

    343. Integer Break

--- leetcode/company/Tencent.js
+++ leetcode/company/Tencent.js
@@ -2,13 +2,13 @@
  * Created by heben on 2017/4/2.
  */
 
-
+//一个整数分解成若干个整数
 const integer_decomposition=function(n){
     let count=[];
     function recursion(m,k){
         count[m] = 0;
-        if(m==k+1) return 1;
-        for(let i=k;i<m;i++){
+        if(m==0) return 1;
+        for(let i=k;i<=m;i++){
             count[m]+=recursion(m-i,i);
         }
         return count[m];
@@ -17,3 +17,49 @@ const integer_decomposition=function(n){
     return count[n];
 };
 console.log(integer_decomposition(5));
+console.log(integer_decomposition(6));
+console.log(integer_decomposition(7));
+console.log(integer_decomposition(8));
+
+
+const integer_decomposition2=function(n){
+    let dp=[],result=0;
+    for(let i=1;i<=n;i++){
+        dp[i]=[];
+        for(let j=n;j>0;j--){
+            if(j>i) dp[i][j]=0;
+            else if(j==i) dp[i][j]=1;
+            else if(j<i) dp[i][j] = dp[i][j+1] + dp[i-j][j];
+        }
+    }
+    for(let i=1;i<=n;i++){
+        result+=dp[n][i];
+    }
+    return dp[n][1];
+};
+console.log(integer_decomposition2(5));
+console.log(integer_decomposition2(6));
+console.log(integer_decomposition2(7));
+console.log(integer_decomposition2(8));
+
+//一个整数分解成若干个连续整数之和
+const integer_decomposition3=function(n){
+    let result = [],count=0;
+    for(let k=2;k<=n/2+1;k++){  //k位分解的个数
+        let temp=2*n/k-k+1;
+        if(temp<=0) break;
+        if(temp%2 == 0){
+            i=temp/2;
+            result[count]=[];
+            for(let j=i;j<=i+k-1;j++){
+                result[count].push(j);
+            }
+            count++;
+        }
+    }
+    return result;
+};
+console.log(integer_decomposition3(15));
+console.log(integer_decomposition3(16));
+console.log(integer_decomposition3(17));
+console.log(integer_decomposition3(18));
